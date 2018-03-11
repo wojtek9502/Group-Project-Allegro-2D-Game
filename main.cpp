9fc80b5bf76b28f72ef37779b7576820c513012b
wojtek <wojtek.klusek2@gmail.com>
2018-03-11 19:37:19 +0100

    fix cat image resolution
    add cat and dog images to menu

--- main.cpp
+++ main.cpp
@@ -20,16 +20,22 @@ int main(){
         cout<<"failed to load display";
 
     ALLEGRO_BITMAP *start_background = load_bitmap("start_background.png");
+    ALLEGRO_BITMAP *dog = load_bitmap("dog.png");
+    ALLEGRO_BITMAP *cat = load_bitmap("cat.png");
 
 
     while(!al_key_down(&keyboard, ALLEGRO_KEY_ESCAPE)){
     al_get_keyboard_state(&keyboard);
 
     al_draw_bitmap(start_background,0,0,0);
+    al_draw_bitmap(dog,100,300,0);
+    al_draw_bitmap(cat,500,300,0);
     al_flip_display();
 
     }
 
+    al_destroy_bitmap(dog);
+    al_destroy_bitmap(cat);
     al_destroy_bitmap(start_background);
     al_destroy_display( display );
     return 0;
--- system_functions.hpp
+++ system_functions.hpp
@@ -2,6 +2,7 @@
 #define SYSTEM_FUNCTIONS_HPP_INCLUDED
 
 
+//example:  ALLEGRO_BITMAP *start_background = load_bitmap("start_background.png");
 ALLEGRO_BITMAP* load_bitmap(char *bitmap_path)
 {
     ALLEGRO_BITMAP *tmp_bitmap_array =  al_load_bitmap(bitmap_path);
