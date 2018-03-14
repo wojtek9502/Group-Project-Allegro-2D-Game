1f8f60c69729544698d19baae6225c89d53b1b5f
wojtek <wojtek.klusek2@gmail.com>
2018-03-14 17:45:02 +0100

    Code refactor

---
+++ game.hpp
@@ -0,0 +1,28 @@
+#ifndef GAME_HPP_INCLUDED
+#define GAME_HPP_INCLUDED
+using namespace std;
+
+void menu_start(ALLEGRO_MOUSE_STATE mouse, ALLEGRO_DISPLAY* display, ALLEGRO_MOUSE_CURSOR* cursor)
+{
+     if((mouse.x >=300 && mouse.x <=500) && (mouse.y >= 150 && mouse.y <=201)){
+
+            printf("one player: (%d, %d)\n", mouse.x, mouse.y);
+            al_set_mouse_cursor(display, cursor);
+            al_rest(0.1);
+            if(mouse.buttons & 1){
+                cout<<"clicked one player: x:"<<mouse.x<<"y: "<<mouse.y<<endl;
+            }
+        }
+
+        if((mouse.x >=300 && mouse.x <=500) && (mouse.y >=230 && mouse.y <=281)){
+            printf("two player: (%d, %d)\n", mouse.x, mouse.y);
+            al_set_mouse_cursor(display, cursor);
+            al_rest(0.1);
+            if(mouse.buttons & 1){
+                cout<<"clicked one player: x:"<<mouse.x<<"y: "<<mouse.y<<endl;
+            }
+        }
+}
+
+
+#endif // GAME_HPP_INCLUDED
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "allegro5/allegro_font.h"
 #include "allegro5/allegro_ttf.h"
 #include "system_functions.hpp"
+#include "game.hpp"
 #define width 800
 #define height 537
 
@@ -34,39 +35,21 @@ int main(){
 
 
 
-    while(!al_key_down(&keyboard, ALLEGRO_KEY_ESCAPE)){
-    al_get_keyboard_state(&keyboard);
-    al_get_mouse_state(&mouse);
-       al_set_system_mouse_cursor(display, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT);
+    while(!al_key_down(&keyboard, ALLEGRO_KEY_ESCAPE))
+    {
+        al_get_keyboard_state(&keyboard);
+        al_get_mouse_state(&mouse);
+        al_set_system_mouse_cursor(display, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT);
 
+        run_menu_start(mouse,display,cursor);
 
-        if((mouse.x >=300 && mouse.x <=500) && (mouse.y >= 150 && mouse.y <=201)){
-
-            printf("one player: (%d, %d)\n", mouse.x, mouse.y);
-            al_set_mouse_cursor(display, cursor);
-            al_rest(0.1);
-            if(mouse.buttons & 1){
-                cout<<"clicked one player: x:"<<mouse.x<<"y: "<<mouse.y<<endl;
-            }
-        }
-
-        if((mouse.x >=300 && mouse.x <=500) && (mouse.y >=230 && mouse.y <=281)){
-            printf("two player: (%d, %d)\n", mouse.x, mouse.y);
-            al_set_mouse_cursor(display, cursor);
-            al_rest(0.1);
-            if(mouse.buttons & 1){
-                cout<<"clicked one player: x:"<<mouse.x<<"y: "<<mouse.y<<endl;
-            }
-        }
-
-    al_draw_bitmap(start_background,0,0,0);
-    al_draw_bitmap(dog,130,390,0);
-    al_draw_bitmap(cat,500,380,0);
-    al_draw_bitmap(one_player, 320, 150, 0);
-    al_draw_bitmap(two_player, 300, 230, 0);
-    al_flip_display();
-
-  }
+        al_draw_bitmap(start_background,0,0,0);
+        al_draw_bitmap(dog,130,390,0);
+        al_draw_bitmap(cat,500,380,0);
+        al_draw_bitmap(one_player, 320, 150, 0);
+        al_draw_bitmap(two_player, 300, 230, 0);
+        al_flip_display();
+    }//END GAME LOOP
 
     al_destroy_bitmap(cursor_bitmap);
     al_destroy_bitmap(two_player);
